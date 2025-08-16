a86edd651be799dbb187b2b488c2d3dea181a76f
rushi <rushikeshdivase96s@gmail.com>
2025-08-16 11:20:25 +0530

    New Files added Hello.jsx

--- new-demo-app-11/src/main.jsx
+++ new-demo-app-11/src/main.jsx
@@ -1,10 +1,11 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import Hello from './Hello'
 
 
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-
+    <Hello />
   </StrictMode>
 )
